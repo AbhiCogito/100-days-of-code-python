cbf602cdcf5bb477c9bb47239830771f8948adfa
Abhineet <singh.abhineet@gmail.com>
2025-05-17 15:06:57 +0530

    Remove duplicate functions

--- Week 2/Ceasar Cipher/Ceasar Cipher with Modulo fxn.py
+++ Week 2/Ceasar Cipher/Ceasar Cipher with Modulo fxn.py
@@ -4,9 +4,7 @@ os.system("clear")
 
 letters = list(string.ascii_lowercase)
 
-def encrypt():
-    message = input("Enter the message that is to be encrypted: ").rstrip()
-    shift = int(input("Enter the shift index: "))
+def encrypt(message,shift):
     encrypted_message = []
     for x in message:
         for index, y in enumerate(letters):
@@ -14,24 +12,17 @@ def encrypt():
                 encrypted_message.append(letters[(index + shift)%26])
     return(''.join(encrypted_message))
 
-def decrypt():
-     message = input("Enter the message that is to be decrypted: ").rstrip()
-     shift = int(input("Enter the shift index: "))
-     encrypted_message = []
-     for x in message:
-        for index, y in enumerate(letters):
-            if x == y:
-                encrypted_message.append(letters[(index - shift)%26])
-     return(''.join(encrypted_message))
-
 def cipher():
     print("Welcome to the Cipher Lab!!")
-    work = input("Type 'encode' to encrypt and 'decode' to decipher: ").rstrip().lower()
-    if work == "encode" or work == "decode":
-        if work == "encode":
-            print(f"The excrypted message is {encrypt()}")
-        else:
-            print(f"The decrypted message is {decrypt()}")
+    work = input("Type 'encode' or 'e' to encrypt and 'decode' or 'd' to decrypt: ").rstrip().lower()
+    if work == "encode" or work == "e":
+        message = input("Enter the message that is to be encrypted: ").rstrip()
+        shift = int(input("Enter the shift index: "))
+        print(f"The encrypted message is {encrypt(message, shift)}")
+    elif work == "decode" or work == "d":
+        message = input("Enter the message that is to be decrypted: ").rstrip()
+        shift = int(input("Enter the shift index: "))
+        print(f"The decrypted message is {encrypt(message, -shift)}")
     else:
         print("Incorrect option selected. \n")
 cipher()
