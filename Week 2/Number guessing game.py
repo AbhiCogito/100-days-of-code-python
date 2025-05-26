64f98bedd87e517a1e74e4c622cfc5f8e68c3e96
Abhineet <singh.abhineet@gmail.com>
2025-05-26 11:09:31 +0530

    Number Guessing Game

---
+++ Week 2/Number guessing game.py
@@ -0,0 +1,44 @@
+import os
+import random
+os.system("clear")
+
+number = random.choice(range(1,100))
+counter = 0
+guesses = []
+
+mode = input("Enter 'e' for easy (10) attempts or 'h' for hard (5) attempts: ")
+
+if mode == 'e':
+    counter = 10
+elif mode == 'h':
+    counter = 5
+
+def game(counter, number):
+
+    while counter > 0:
+        
+        while True:
+            guess = int(input("Please enter your guess: "))
+            if guess in guesses:
+                print("You have already guessed this number.")
+            else:
+                guesses.append(guess)
+                break
+
+        if guess == number:
+            print(f"You won. The correct number is {guess}")
+            return counter
+        elif guess < number:
+            counter -= 1
+            print("Low")
+        elif guess > number:
+            counter -= 1
+            print("High")
+
+    return counter
+
+counter = game(counter, number)
+
+if counter == 0:
+    print("You have exhausted all your attempts.")
+    print(f"The correct number is {number}")
