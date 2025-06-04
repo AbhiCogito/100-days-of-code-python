a8508385c737fea5ed0ee188601eec75ad0a86ee
Abhineet <singh.abhineet@gmail.com>
2025-06-04 16:04:26 +0530

    Week3

---
+++ Week 3/OOP concepts.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.mainloop()
+
+from turtle import Turtle, Screen
+
+
+
+donatello = Turtle()
+print(donatello)
+donatello.shape("turtle")
+donatello.color("cyan")
+
+
+
+display = Screen()
+print(display.canvheight)
+display.exitonclick()
