6f78c2ef2e60d32854cbb69b3e40f184aa721474
Abhineet <singh.abhineet@gmail..com>
2025-06-10 12:01:55 +0530

    Coffee makker prog using Angels's code

--- Week 3/coffee/oop-coffee-machine-start/main.py
+++ Week 3/coffee/oop-coffee-machine-start/main.py
@@ -1,9 +1,29 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+import os
+
+os.system("clear")
 
 display = Menu()
 coffee = CoffeeMaker()
-accounts = MoneyMachine()
+account = MoneyMachine()
+is_on = True
 
-print(display)
+
+while is_on:
+    print("Welcome to Starbucks!!")
+    print(f"We have the following items: {display.get_items()}")
+    choice = input("Please enter your selected drink: ").lower()
+    
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee.report()
+        account.report()
+    else:
+        drink = display.find_drink(choice)
+        if coffee.is_resource_sufficient(drink) and account.make_payment(drink.cost):
+            coffee.make_coffee(drink)
+
+
