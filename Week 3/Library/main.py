bf999ea6a7acece89638c7f2d0161da35bfd26ba
Abhineet <singh.abhineet@gmail.com>
2025-06-13 10:00:38 +0530

    Library using Classes

--- Week 3/Library/main.py
+++ Week 3/Library/main.py
@@ -1,4 +1,6 @@
 from Books import book_data
+from tabulate import tabulate
+import os
 
 library = []
 
@@ -29,14 +31,15 @@ class Book:
         return f"Library has received the {self.title} book."
 
 for book in book_data:
-    title = book["title"]
-    author = book["author"]
-    genre = book["genre"]
-    year = book["year"]
-    book_add = Book(title, author, genre, year)
+    # title = book["title"]
+    # author = book["author"]
+    # genre = book["genre"]
+    # year = book["year"]
+    # book_add = Book(title, author, genre, year)
+    book_add = Book(**book)  # ✅ Dictionary unpacking
     library.append(book_add)
 
-class Library:
+class Library_setup:
     def __init__(self, my_library):
         self.books = my_library
 
@@ -46,12 +49,52 @@ class Library:
 
     def borrow_book(self, book_name, user):
         for book in self.books:
-            if book.title == book_name:
+            if book.title.lower() == book_name.lower():
                 return book.check_out(user)
         return f"{book_name} is not present in the library."
 
     def return_book(self, book_name):
         for book in self.books:
-            if book.title == book_name:
+            if book.title.lower() == book_name.lower():
                 return book.return_book()
             
+#Write the main loop        
+
+my_library = Library_setup(library)
+os.system("clear")
+
+print("Welcome to the library.")
+user = input("Please enter your name: ").lower().strip()
+
+while True:
+    print("1. View all books.\
+           2. Borrow a book \
+           3. Return a book\
+           4. Exit ")
+
+    try:
+        choice = int(input("Please select an option (1–4): ").strip())
+    except ValueError:
+        print("Invalid input. Please enter a number between 1 and 4.")
+        continue
+
+    if choice in (1,2,3,4):
+        if choice == 1:
+            table_data = []
+            for book in my_library.books:
+                status = "Available" if book.available else f"Lent to {book.user}"
+                table_data.append([book.title, book.genre, book.author, book.year, status])
+    
+            headers = ["Title", "Genre", "Author", "Year", "Status"]
+            print("\nBook Catalog:\n")
+            print(tabulate(table_data, headers=headers, tablefmt="fancy_grid"))
+        elif choice == 2:
+            borrow_book_name = input("Please enter the name of the book you want to borrow: ").lower().strip()
+            print(my_library.borrow_book(borrow_book_name, user))
+        elif choice == 3:
+            return_book_name = input("Please enter the name of the book you want to return: ").lower().strip()
+            print(my_library.return_book(return_book_name))
+        elif choice == 4:
+            break
+    else: 
+        print("Invalid option selected.")
