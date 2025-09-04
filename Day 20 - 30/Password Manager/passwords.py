400c0d9b8831d91217111e5ed55250342df43887
Abhineet <singh.abhineet@gmail.com>
2025-09-04 11:52:39 +0530

    Password manager with JSON

--- Day 20 - 30/Password Manager/passwords.py
+++ Day 20 - 30/Password Manager/passwords.py
@@ -1,9 +1,9 @@
-import os, csv, random, string
+import os, csv, random, string, json
 import tkinter as tk
 from tkinter import ttk, messagebox
 
 file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logo.png")
-csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "credentials.csv")
+json_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "credentials.json")
 os.system("clear")
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -21,7 +21,7 @@ c.grid(column=1, row=0, pady= 30) #Placing the canvas itself using the grid meth
 p_category = tk.Label(text="Category", font=("Calibri", 15, "bold"))
 p_category.grid(column=0, row=2)
 p_length = tk.Label(text="Password Length", font=("Calibri", 15, "bold"))
-p_length.grid(column=0, row=3)
+p_length.grid(column=0, row=4)
 
 #Adding space between all the rows
 for i in range(6):
@@ -43,7 +43,7 @@ slider = tk.Scale(
     length=200,         # slider length in pixels
     tickinterval=1      # show tick marks every 1 unit
 )
-slider.grid(column=1, row=3, sticky='w')
+slider.grid(column=1, row=4, sticky='w')
 
 '''
 # Password composition frame
@@ -61,6 +61,10 @@ tk.Entry(composition_frame, width=5).grid(row=1, column=2, padx=5)
 '''
 
 # ---- Email/Username ----
+tk.Label(root, text="Website").grid(row=3, column=0, padx=5, pady=5, sticky="e") #stick to "east"
+web_var = tk.StringVar() #Tkinter string variable to store user's input
+tk.Entry(root, textvariable=web_var).grid(row=3, column=1, padx=5, pady=5, sticky="w") #stick to "west"
+
 tk.Label(root, text="Email:").grid(row=5, column=0, padx=5, pady=5, sticky="e") #stick to "east"
 email_var = tk.StringVar() #Tkinter string variable to store user's input
 tk.Entry(root, textvariable=email_var).grid(row=5, column=1, padx=5, pady=5, sticky="w") #stick to "west"
@@ -95,6 +99,14 @@ def store_data():
     email = email_var.get()
     password = password_var.get()
     category = category_var.get()
+    website = web_var.get()
+
+    #Defining the structure of data for storing
+    json_dict = {website: {
+                    "Category" : category,
+                    "Email"    : email,
+                    "Password" : password
+    }}
 
     if email == "" or password == "" or category == "":
         messagebox.showinfo(message="Category, Email & Password can't be empty.")
@@ -102,15 +114,54 @@ def store_data():
         save = messagebox.askokcancel(title=category, message=f"Confirm for saving: \n \
                                                                 Email: {email} \n Password: {password}")
         if save:
-            with open(csv_path, 'a', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerow([category, email, password])
-                email_var.set("")
-                password_var.set("")
-                category_var.set("")
+            try:
+                with open(json_path, "r") as file:
+                    #Load the JSON data for reading
+                    json_data = json.load(file)
+            except (FileNotFoundError, json.JSONDecodeError):
+                # if file doesn’t exist or is empty → start with empty dict
+                json_data = {}
+
+            # update existing data
+            json_data.update(json_dict)
+
+            # write back to file
+            with open(json_path, "w") as file:
+                json.dump(json_data, file, indent=4)
+            email_var.set("")
+            password_var.set("")
+            category_var.set("")
+            web_var.set("")
+
+def search_data():
+    website = web_var.get().strip().lower()
+
+    if website == "":
+        messagebox.showinfo(message="Website can't be empty.")
+        return #To exit the loop as no point continuing
+    else:
+        try:
+            with open(json_path, "r") as file:
+                reader = json.load(file)
+                for site, details in reader.items():
+                    if site.lower() == website:
+                        category = details.get("Category", "")
+                        email = details.get("Email", "")
+                        password = details.get("Password", "")
+                        messagebox.showinfo(title=site, message=f"Category: {category}\n \
+                                                                Email: {email}\n \
+                                                                Password: {password}  ")
+                        
+                        return
+        except (FileNotFoundError, json.JSONDecodeError):
+            messagebox.showinfo(message="No data file exists. Save some passwords before sarching!!")
+
 
 generate_btn = tk.Button(root, text="Generate", command=generate_password)
 generate_btn.grid(row=6, column=2, padx=5, pady=5)
+
+search_btn = tk.Button(root, text="Search Data", width=20, command=search_data)
+search_btn.grid(row=3, column=2, columnspan=2, padx=5, pady=5)
 
 # ---- Store Data ----
 store_btn = tk.Button(root, text="Store Data", width=20, command=store_data)
