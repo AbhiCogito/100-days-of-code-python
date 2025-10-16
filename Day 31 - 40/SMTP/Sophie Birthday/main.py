7b0f0aa9453a95642988e357ee7b03317147270d
Abhineet <singh.abhineet@gmail.com>
2025-10-16 17:44:51 +0530

    Bdy email SIP amount

--- Day 31 - 40/SMTP/Sophie Birthday/main.py
+++ Day 31 - 40/SMTP/Sophie Birthday/main.py
@@ -4,6 +4,7 @@ import smtplib
 my_email = "abhineet.exam@gmail.com"
 my_password = "daicakkpbexfnaji"
 send_email = ['gk4ias@gmail.com', 'hcl.priya23@gmail.com']
+message = "Invest "
 
 sophie_bdy = dt.date(2020, 10, 25)
 milestone = [i for i in range (100, 50000, 100)]
@@ -11,7 +12,7 @@ today = dt.datetime.now()
 
 diff = today.date() - sophie_bdy
 diff_days = diff.days
-# diff_month = diff.
+diff_year = today.year - sophie_bdy.year
 
 def birthday_email(days):
     connection = smtplib.SMTP('smtp.gmail.com', 587)
@@ -19,7 +20,9 @@ def birthday_email(days):
     connection.starttls()
     connection.login(user=my_email, password=my_password)
     connection.sendmail(from_addr=my_email, to_addrs=send_email, 
-                        msg=f"Subject: Sophie is {days} old!! Celebrate!!! \n\n Sophie turns {days} days old tomorrow.")
+                        msg=f"Subject: Sophie is {days} days old!! Celebrate!!! \n\n" \
+                        f"Sophie turns {days} days old tomorrow. \n" \
+                        f"Invest {diff_year*1000} per month in mutual funds.")
     connection.close()
 
 if (diff_days + 1) in milestone:
