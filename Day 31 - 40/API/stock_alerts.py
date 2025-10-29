1bde352fb4f16e23723d15bc3d36bbc305fda221
Abhineet <singh.abhineet@gmail.com>
2025-10-29 11:22:56 +0530

    Stock Prices Tracker

--- Day 31 - 40/API/stock_alerts.py
+++ Day 31 - 40/API/stock_alerts.py
@@ -3,14 +3,16 @@ from datetime import datetime, timedelta
 import os, json, smtplib
 from dotenv import load_dotenv
 os.system("clear")
-
 load_dotenv()
+
+#-----Getting from and to dates to fetch stock prices-------#
 today = datetime.today()
 from_date = (today - timedelta(10)).strftime('%Y-%m-%d')
 to_date = today.strftime('%Y-%m-%d')
 
 
-STOCK = ['AAPL', 'MSFT', 'NVDA', 'TSLA', 'AMZN']
+# STOCK = ['AAPL', 'MSFT', 'NVDA', 'TSLA', 'AMZN']
+STOCK = ['TSLA']
 COMPANY_NAME = "Tesla Inc"
 DIGEST = ""
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
@@ -27,7 +29,7 @@ def send_email():
     connection.starttls()
     connection.login(user=my_email, password=my_password)
     connection.sendmail(from_addr=my_email, to_addrs="gk4ias@gmail.com", 
-                        msg=f"Subject: Stock price movement \n\n{DIGEST}")
+                        msg=f"Subject: Stock price movement \n\n{DIGEST}".encode('utf-8'))
     connection.close()
 
 def get_stock_news(stock):
@@ -45,11 +47,11 @@ def get_stock_news(stock):
     response.raise_for_status()
     data = response.json()
 
+    #--------------Get only the first 3 articles(source+title)------------#
     news = ""
     for article in data.get("articles", [])[:3]:
-        source = article.get("source", {}).get("name", "Unknown Source")
-        title = article.get("title", "No Title")
-        print(f"{source} - {title}")
+        source = article.get("source", {}).get("name", "Unknown Source") #????????
+        title = article.get("title", "No Title") #?????????
         news += f"{source} - {title} \n"
     # print(json.dumps(data, indent=4))
 
@@ -67,29 +69,33 @@ def get_stock_prices(stock):
     response = rq.get(url=STOCK_ENDPOINT ,params=STOCK_PARAMS)
     response.raise_for_status()
     data = response.json()
+
+    #-------Get data from the last trading day---------#
     time_series = data.get("Time Series (Daily)", {})  
     dates = sorted(time_series.keys(), reverse=True)
-
     latest, prev = dates[0], dates[1]
     latest_close = float(time_series[latest]["4. close"])
     prev_close = float(time_series[prev]["4. close"])
     change = ((latest_close - prev_close) / prev_close) * 100
 
-    if change > 5:
-        print(f"{stock} - {change}%")
+    if change:
+        print(f"{stock} - {change:+.2f}%")
         news = get_stock_news(stock)
-        DIGEST += f"{stock} ({change:.2f}% change)\n{news}\n{'-'*60}\n"
+        DIGEST += f"{stock} ({change:+.2f}% change)\n{news}\n{'-'*180}\n"
 
 for s in STOCK:
     get_stock_prices(s)
 
+#------Send email only when there is some content---------#
 if DIGEST.strip():
     send_email()
-    print("✅ Email sent.")
+    print("Email sent.")
+    print(DIGEST)
 else:
-    print("ℹ️ No significant stock movement found.")
+    print("No significant stock movement found.")
+    print(DIGEST)
 
-# get_stock_news("TSLA")
+
 
 ## STEP 1: Use https://newsapi.org/docs/endpoints/everything
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
