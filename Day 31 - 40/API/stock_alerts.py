f79c0ccfa4fec4a1e2e7f3d9f2d9d2a7e3d1c24e
Abhineet <singh.abhineet@gmail.com>
2025-10-29 11:33:36 +0530

    Share Prices Tracker

--- Day 31 - 40/API/stock_alerts.py
+++ Day 31 - 40/API/stock_alerts.py
@@ -49,10 +49,10 @@ def get_stock_news(stock):
     #--------------Get only the first 3 articles(source+title)------------#
     news = ""
     for article in data.get("articles", [])[:3]:
-        source = article.get("source", {}).get("name", "Unknown Source") #????????
-        title = article.get("title", "No Title") #?????????
+        source = article.get("source", {}).get("name", "Unknown Source") 
+        title = article.get("title", "No Title") 
         news += f"{source} - {title} \n"
-    # print(json.dumps(data, indent=4))
+    print(json.dumps(data, indent=4))
 
     return news
 
@@ -89,26 +89,5 @@ for s in STOCK:
 if DIGEST.strip():
     send_email()
     print("Email sent.")
-    print(DIGEST)
 else:
     print("No significant stock movement found.")
-    print(DIGEST)
-
-
-
-## STEP 1: Use https://newsapi.org/docs/endpoints/everything
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-#HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
-#HINT 2: Work out the value of 5% of yerstday's closing stock price. 
-
-
-
-## STEP 2: Use https://newsapi.org/docs/endpoints/everything
-# Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
-#HINT 1: Think about using the Python Slice Operator
-
-
-
-## STEP 3: Use twilio.com/docs/sms/quickstart/python
-# Send a separate message with each article's title and description to your phone number. 
-#HINT 1: Consider using a List Comprehension.
