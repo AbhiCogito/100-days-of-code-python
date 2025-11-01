c76a732e65a56ff548f16d27c33207f253abde6d
Abhineet <singh.abhineet@gmail.com>
2025-11-01 09:38:14 +0530

    json saved to gsheets via gspread

--- Capstone_Projects/GymTracker/src/helpers/save_to_gsheets.py
+++ Capstone_Projects/GymTracker/src/helpers/save_to_gsheets.py
@@ -1,5 +1,5 @@
-import os
-import gspread 
+import os, json, gspread
+import pandas as pd
 from google.oauth2.service_account import Credentials
 from dotenv import load_dotenv
 load_dotenv()
@@ -7,26 +7,29 @@ os.system("clear")
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(base_dir, os.pardir, os.pardir))
+source_file = os.path.abspath(os.path.join(project_root, "data", "hevy_data_clean.json"))
 cred_path = os.getenv("GOOGLE_SHEETS_CREDENTIALS")
 GOOGLE_CREDENTIALS_PATH = os.path.join(project_root, cred_path)
-
-print(GOOGLE_CREDENTIALS_PATH)
 
 scopes = ["https://www.googleapis.com/auth/spreadsheets"]
 creds = Credentials.from_service_account_file(GOOGLE_CREDENTIALS_PATH, scopes=scopes)
 client = gspread.authorize(creds)
 sheet_id = "1tG_iFU4gSw2ajdjIMeubWnt-wMIiNvkavABO_wLvN_U"
-sheet = client.open_by_key(sheet_id)
+sh = client.open_by_key(sheet_id)
 
-values_list = sheet.sheet1.row_values(1)
-worksheet = sheet.worksheet('2025')
+with open(source_file, 'r') as f:
+    data = json.load(f)
 
-# Example data
-data = [
-    ["Date", "Workout", "Duration", "Effort"],
-    ["2025-10-30", "Chest + Shoulders", "75 mins", "All Out"]
-]
+df = pd.DataFrame(data)
+df = df.fillna("") #Fix NaN values as gspread doesn't accept NaN
+headers = df.columns.tolist()
+data_rows = df.values.tolist()
+data_dump = [headers] + data_rows
 
-# worksheet.update('A1', data)
-print("✅ Data written successfully!")
+worksheet = sh.worksheet('2025')
+worksheet.update('A1', data_dump)
+
+
+
+
 
