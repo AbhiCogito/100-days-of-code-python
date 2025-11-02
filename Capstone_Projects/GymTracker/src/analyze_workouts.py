0c13467e244024dadce226a9f1195b3542db7408
Abhineet <singh.abhineet@gmail.com>
2025-11-02 09:46:50 +0530

    exercise analysis using pandas

--- Capstone_Projects/GymTracker/src/analyze_workouts.py
+++ Capstone_Projects/GymTracker/src/analyze_workouts.py
@@ -7,6 +7,9 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(base_dir, os.pardir))
 source_file = os.path.abspath(os.path.join(project_root, "data", "hevy_data_clean.json"))
 
+#Defines how many results per query is needed. Say 5 days per month.
+RESULTS_SIZE = 3
+
 class WorkoutAnalyzer:
     def __init__(self, source_file):
         with open(source_file, 'r') as f:
@@ -17,6 +20,7 @@ class WorkoutAnalyzer:
         self.df['Date'] = pd.to_datetime(self.df['Date'])
         self.df['Week'] = self.df['Date'].dt.isocalendar().week
         self.df['Month'] = self.df['Date'].dt.month
+        self.df['Year'] = self.df['Date'].dt.year
 
     def vol_per_exercise(self):
         return(
@@ -81,51 +85,133 @@ class WorkoutAnalyzer:
         )
     
     def top_day_in_week(self, week):
-
+        #Copy the data for the relevant week
         week_data = self.df[self.df['Week'] == week].copy()
-
+        #Find sum of Total Vol grouped by Date & Title
         daily_totals = (
             week_data.groupby(['Date', 'Title'])['Total Vol']
             .sum()
             .reset_index()
-    )
+        ) #Groupby output will only contain Date, Title, and Total Vol. Other cols will be dropped
 
-        sorted_data = daily_totals.sort_values(by='Total Vol', ascending=False).iloc[:3]
-        data_row = sorted_data.head(1).copy()
+        #Arrange top three summed values in descending order
+        data_row = daily_totals.sort_values(by='Total Vol', ascending=False).iloc[:RESULTS_SIZE]
+
+        #Convert month display format from 09 to Sep
         data_row['Month'] = data_row['Date'].dt.strftime('%b')
         data_row['Week'] = week
 
+        #Find duration value in the original copy as data is lost after groupby
         duration_lookup = week_data[['Date', 'Title', 'Duration']].drop_duplicates()
+        #Merge duration back to the data set arranged in descending order
         data_row = data_row.merge(duration_lookup, on=['Date', 'Title'], how='left')
 
         return data_row[['Month','Week', 'Date', 'Title', 'Duration', 'Total Vol']]
     
     def top_day_in_month(self, month):
-
+        #Copy the data for the relevant month
         month_data = self.df[self.df['Month'] == month].copy()
-        sorted_data = month_data.sort_values(by='Total Vol', ascending=False)
-        data_row = sorted_data.head(1).copy()
+        #Find sum of Total Vol by Date & Title
+        monthly_totals = (
+            month_data.groupby(['Date', 'Title'])['Total Vol']
+            .sum()
+            .reset_index()
+        )
+
+        data_row = monthly_totals.sort_values(by='Total Vol', ascending=False).iloc[:RESULTS_SIZE]
         data_row['Month'] = data_row['Date'].dt.strftime('%b')
+        data_row['Week'] = data_row['Date'].dt.isocalendar().week
+
+        #Find duration value in the original copy as data is lost after groupby
+        duration_lookup = month_data[['Date', 'Title', 'Duration']].drop_duplicates()
+        #Merge duration back to the data set arranged in descending order
+        data_row = data_row.merge(duration_lookup, on=['Date', 'Title'], how='left')
 
         return data_row[['Month','Week', 'Date', 'Title', 'Duration', 'Total Vol']]
     
     def top_week_in_month(self, month):
+        #Copy the data for the relevant month
         month_data = self.df[self.df['Month'] == month].copy()
-        sorted_data = month_data.sort_values(by='Total Vol', ascending=False)
-        data_row = sorted_data.head(1).copy()
-        data_row['Month'] = data_row['Date'].dt.strftime('%b')
+        #Find sum of Total Vol by Week & Title
+        monthly_totals = (
+            month_data.groupby('Week')['Total Vol']
+            .sum()
+            .reset_index()
+        )
 
-        return data_row[['Month','Week', 'Date', 'Title', 'Duration', 'Total Vol']]
+        #Find sum of total duration per week
+        duration = (
+            month_data.groupby('Week')['Duration']
+            .sum()
+            .reset_index()
+        )
+
+        #Merge values by week
+        merged = monthly_totals.merge(duration, on='Week')
+        data_row = merged.sort_values(by='Total Vol', ascending=False).iloc[:RESULTS_SIZE] #.copy()
+
+        #Strftime needs date in YYYY-MM-DD format
+        data_row['Month'] = pd.to_datetime(f'2025-{month}-01').strftime('%b')
+
+        return data_row[['Month','Week', 'Duration', 'Total Vol']]
    
-    # def top_month_in_year(self, year):
+    def top_month_in_year(self, year):
+        #Copy the data for the relevant month
+        year_data = self.df[self.df['Year'] == year].copy()
+        #Find sum of Total Vol by Week & Title
+        monthly_totals = (
+            year_data.groupby('Month')['Total Vol']
+            .sum()
+            .reset_index()
+        )
+
+        #Find sum of total duration per week
+        duration = (
+            year_data.groupby('Month')['Duration']
+            .sum()
+            .reset_index()
+        )
+
+        #Merge values by week
+        merged = monthly_totals.merge(duration, on='Month')
+        data_row = merged.sort_values(by='Total Vol', ascending=False).iloc[:RESULTS_SIZE] #.copy()
+
+        #Strftime needs date in YYYY-MM-DD format
+        data_row['Month'] = pd.to_datetime(data_row['Month'], format='%m').dt.strftime('%b')
+        data_row['Year'] = year
+
+        return data_row[['Year', 'Month', 'Duration', 'Total Vol']]
 
     # def top_5_days_in_month(self, month):
 
-    # def top_5_days_in_year(self, year):
+    def top_day_in_year(self, year):
+        # Copy the data for the relevant year
+        year_data = self.df[self.df['Year'] == year].copy()
 
+        # Find sum of Total Vol by Date & Title
+        yearly_totals = (
+            year_data.groupby(['Date', 'Title'])['Total Vol']
+            .sum()
+            .reset_index()
+        )
+
+        # Arrange top three summed values in descending order
+        data_row = yearly_totals.sort_values(by='Total Vol', ascending=False).iloc[:RESULTS_SIZE]
+
+        # Add Month and Week info from Date
+        data_row['Month'] = data_row['Date'].dt.strftime('%b')
+        data_row['Week'] = data_row['Date'].dt.isocalendar().week
+
+        # Find duration value in the original copy as data is lost after groupby
+        duration_lookup = year_data[['Date', 'Title', 'Duration']].drop_duplicates()
+
+        # Merge duration back to the data set arranged in descending order
+        data_row = data_row.merge(duration_lookup, on=['Date', 'Title'], how='left')
+
+        return data_row[['Month', 'Week', 'Date', 'Title', 'Duration', 'Total Vol']]
 
 
 analyzer = WorkoutAnalyzer(source_file)
-result = analyzer.top_day_in_week(42)
+result = analyzer.top_month_in_year(2025)
 # result = analyzer.top_day_in_month(10)
 print(result)
