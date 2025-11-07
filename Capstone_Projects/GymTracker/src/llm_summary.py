9cf9f9da948bb355968e0200fe18cb245e1db14e
Abhineet <singh.abhineet@gmail.com>
2025-11-07 12:16:43 +0530

    Token usage when accessing LLM via API

--- Capstone_Projects/GymTracker/src/llm_summary.py
+++ Capstone_Projects/GymTracker/src/llm_summary.py
@@ -1,7 +1,6 @@
-import requests, sys, os
+import requests, sys, os, json
 from dotenv import load_dotenv
 from analysis.summary import progress_data
-
 
 load_dotenv()
 os.system("clear")
@@ -9,11 +8,8 @@ os.system("clear")
 GROQ_API = os.getenv("GROQ_LLM_API")
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-
-
 MAX_LENGTH = 5000  
 short_progress_data = str(progress_data)[:MAX_LENGTH]
-# print(progress_data)
 
 # Format prompt
 prompt = f"Analyze this workout progress data and give insights, trends, and suggestions:\n{short_progress_data}"
@@ -31,17 +27,16 @@ payload = {
     ]
 }
 
-# response = requests.post(url, headers=headers, json=payload)
-# print(response.json()["choices"][0]["message"]["content"])
-
 response = requests.post(url, headers=headers, json=payload)
 
 # Check if the response contains the 'choices' key
 response_data = response.json()
 
 if response.status_code == 200:
+    #Most LLMs return a JSON which contains <choices> dictionary. Its presence means response is successful.
     if "choices" in response_data:
-        print(response_data["choices"][0]["message"]["content"])
+        # print(json.dumps(response_data, indent=2)) #To check for tokwn usage
+        print(response_data["choices"][0]["message"]["content"]) #To print only the text output from the model
     else:
         print("No 'choices' key found in the response. Here's the full response:")
         print(response_data)
