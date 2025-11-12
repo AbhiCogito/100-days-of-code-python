6354dcec417264022eec3aa7b539db0f3b03bbfa
Abhineet <singh.abhineet@gmail.com>
2025-11-12 16:59:44 +0530

    Generate charts for workouts

--- Capstone_Projects/GymTracker/src/visuals.py
+++ Capstone_Projects/GymTracker/src/visuals.py
@@ -5,7 +5,7 @@ os.system("clear")
 # Add parent directory to path
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
-from analysis.summary import progress_data
+# from analysis.summary import progress_data
 from analysis.workouts import WorkoutAnalyzer
 from analysis.progress import ProgressAnalyzer
 from matplotlib import pyplot as plt
@@ -14,10 +14,49 @@ from matplotlib import pyplot as plt
 wa = WorkoutAnalyzer(None)
 pa = ProgressAnalyzer(None)
 
-# Get data for plotting
-print(progress_data)
-vol_data = wa.vol_per_exercise()
-print(vol_data)
+class workouts_charts:
+
+    def __init__(self, workouts=None, progress=None):
+
+        self.wa = workouts if workouts else WorkoutAnalyzer(None)
+        self.pa = progress if progress else ProgressAnalyzer(None)
+
+    def vol_per_exercise_chart(self, items=3):
+
+        df = self.wa.vol_per_exercise()
+        vol_data = df.sort_values(by='Total Vol', ascending=False)
+        vol_data = vol_data.head(items).copy()
+
+        fig, ax = plt.subplots()
+        ax.bar(vol_data['Exercise'], vol_data['Total Vol'])
+        ax.set_xlabel('Exercise Name')
+        ax.set_ylabel('Total volume in kg')
+        ax.set_title(f'Total vol for top {items} exercises')
+
+    def vol_per_title_chart(self, items=5):
+
+        df = self.wa.vol_per_title().head(items).copy()
+        print(df)
+
+        fig, ax = plt.subplots()
+        ax.bar(df['Title'], df['Total Vol'])
+        ax.set_xlabel('Day Focus')
+        ax.set_ylabel('Total volume in kg')
+        ax.set_title(f'Total vol for top {items} days')
+        plt.setp(ax.get_xticklabels(),rotation=45, ha='right')
+        plt.show()
+
+wc = workouts_charts(workouts=wa, progress=pa)
+wc.vol_per_title_chart()
+
+# fig, axes = plt.subplots(2, 2)  # 2x2 grid of axes
+# axes[0, 0].plot([1, 2, 3], [1, 4, 9])   # top-left
+# axes[0, 1].bar([1, 2, 3], [3, 2, 5])    # top-right
+# axes[1, 0].scatter([1, 2, 3], [5, 7, 9]) # bottom-left
+# axes[1, 1].pie([10, 20, 30])             # bottom-right
+
+# plt.show()
+
 
 x = ['Money', 'Sophie', 'Priya', 'Anne']
 y = [1,2,3,4]
